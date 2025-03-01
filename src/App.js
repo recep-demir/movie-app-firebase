1b778acf8c9840ce382ad9524fea89e4d2f8bd3f
recep-demir <demir.rp@gmail.com>
2025-03-01 22:42:39 +0100

    add react-toastify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import Register from "./pages/Register";
 import AuthContext from "./context/AuthContext";
 import MovieContext from "./context/MovieContext";
+import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+
 
 const App = () => {
   return (
@@ -10,7 +13,9 @@ const App = () => {
 
       <AuthContext>
         <MovieContext>
-          
+          <AppRouter/>
+          <ToastContainer/>
+
 
 
         </MovieContext>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 const AppRouter = () => {
