bfbc149807db8816371f3bd529d9066c77e11293
recep-demir <demir.rp@gmail.com>
2025-03-02 20:43:06 +0100

    .

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { AuthContextt } from '../context/AuthContext'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRouter = () => {
+
+const{currentUser}=useContext(AuthContextt)
+
+
+  return currentUser ? <Outlet/>: <Navigate to="/"/>
+
+   
+  
+}
+
+export default PrivateRouter
