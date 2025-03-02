02f3b59c8bcd8d1655edefff350459401f0f011b
recep-demir <demir.rp@gmail.com>
2025-03-02 21:12:44 +0100

    edit AppRouter and PrivateRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,26 +6,20 @@ import Register from "../pages/Register";
 import Main from "../pages/Main";
 import PrivateRouter from "./PrivateRouter";
 import MovieDetail from "../pages/MovieDetail";
-// toastContainer de Browser da görünsün diye browserRouter sarmalını üst component olan indexedDB.js de yaptık
+
 const AppRouter = () => {
   return (
     <>
 <Navbar/>
 
 <Routes>
-<Route path="/" element={<Main/>}/>
+  <Route path="/" element={<Main/>}/>
   <Route path="/register" element={<Register/>}/>
   <Route path="/login" element={<Login/>}/>
-
-<Route path="/details/:id" element={<PrivateRouter/>}>
-
+  <Route path="/details/:id" element={<PrivateRouter/>}>
   <Route path="" element={<MovieDetail/>}/>
 </Route>
-
-
 </Routes>
-
-
 
     </>
   )
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -3,14 +3,9 @@ import { AuthContextt } from '../context/AuthContext'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
-
 const{currentUser}=useContext(AuthContextt)
-
-
-  return currentUser ? <Outlet/>: <Navigate to="/"/>
-
-   
   
+return currentUser ? <Outlet/>: <Navigate to="/"/>
 }
 
 export default PrivateRouter
