b78be0695ea218ab9f36502d0f5ef466d22f3171
recep-demir <demir.rp@gmail.com>
2025-03-03 00:29:11 +0100

    edited detail page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
-    //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
     extend: {
       colors: {
         "gray-dark-main": "#23242a",
