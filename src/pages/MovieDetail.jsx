1f4382360a7d88eebb879cc212cac629525424e1
recep-demir <demir.rp@gmail.com>
2025-03-03 01:03:27 +0100

    edit button style

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -15,11 +15,11 @@ useEffect(()=>{
 
    
   return (
-    <div className="md:container px-10 mx-auto py-5">
-      <h1 className="text-center dark:text-white text-3xl">{detay.title}</h1>
+    <div className="md:container px-10 mx-auto py-5  ">
+      <h1 className="text-center text-white font-bold text-3xl mb-3">{detay.title}</h1>
 
-      <div className="md:container flex justify-center px-10">
-        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg">
+      <div className="md:container flex justify-center px-10 ">
+        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-400 shadow-lg">
           <img
             className=" lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
             src={`https://image.tmdb.org/t/p/w1280${detay.poster_path}`}
@@ -27,12 +27,12 @@ useEffect(()=>{
           />
           <div className="p-6 flex flex-col justify-between">
             <div>
-              <h5 className="text-gray-900 text-xl font-medium mb-2">
+              <h5 className=" text-white font-bold text-xl font-medium mb-2">
                 Overview
               </h5>
-              <p className="text-gray-700 text-base mb-4">{detay.overview}</p>
+              <p className="text-black text-base mb-4">{detay.overview}</p>
             </div>
-            <ul className="bg-gray-100 rounded-lg border border-gray-400 text-gray-900">
+            <ul className="bg-gray-600 rounded-lg border border-gray-400 text-gray-900">
               <li className="px-6 py-2 border-b border-gray-400 w-full rounded-t-lg">
                 {"Release Date : " +detay.release_date}
               </li>
@@ -42,18 +42,19 @@ useEffect(()=>{
               <li className="px-6 py-2 border-b border-gray-400 w-full">
                 {"Total Vote : "+ detay.vote_count}
               </li>
-              <li className="px-6 py-2 border-gray-400 w-full rounded-t-lg">
-                <Link
-                  to={-1}
-                  className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
-                >
-                  Go Back
-                </Link>
-              </li>
+
             </ul>
           </div>
         </div>
       </div>
+      <div className='flex justify-center mt-6'>
+        <Link to={-1}
+           className="text-white hover:text-red-700 transition duration-300 ease-in-out mb-4">
+           <button  className="btn-danger-bordered "  >  Go Back </button>
+        </Link>
+
+           </div>
+      
     </div>
   );
 }
